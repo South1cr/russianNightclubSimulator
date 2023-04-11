678fb62d5bba1a6ce9a28d52fc0b0453cb1a6a6d
Connor Southwell <south1cr@gmail.com>
2023-04-10 21:44:54 -0400

    yup

--- js/index.js
+++ js/index.js
@@ -1,53 +1,234 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+canvas.height = window.innerHeight;
+
+let score = 0;
 let gameOn = false;
+let muted = false;
+let animationInterval;
+const audioElement = new Audio("./assets/Vitas - 7th Element (2002) _D.mp3.mp3");
+
+let leftArrowIsPressed = false;
+let upArrowIsPressed = false;
+let downArrowIsPressed = false;
+let rightArrowIsPressed = false;
 
 const arrowLeft = new Image();
-arrowLeft.src ='./assets/staticLeft.png';
+arrowLeft.src = './assets/staticLeft.png';
+const greenLeft = new Image();
+greenLeft.src = './assets/greenLeft.png';
 const arrowUp = new Image();
-arrowUp.src ='./assets/staticUp.png';
+arrowUp.src = './assets/staticUp.png';
+const greenUp = new Image();
+greenUp.src = './assets/greenUp.png';
 const arrowDown = new Image();
-arrowDown.src ='./assets/staticDown.png';
+arrowDown.src = './assets/staticDown.png';
+const greenDown = new Image();
+greenDown.src = './assets/greenDown.png';
 const arrowRight = new Image();
-arrowRight.src ='./assets/staticRight.png';
+arrowRight.src = './assets/staticRight.png';
+const greenRight = new Image();
+greenRight.src = './assets/greenRight.png';
+/*
+const arrowLeftDynamic = new Image()
+arrowLeftDynamic.src = './assets/leftArrowDynamic.png'
+const arrowUpDynamic = new Image();
+arrowUpDynamic.src = "./assets/arrowBlue.gif";
+*/
+const notesArray = [];
+
+const leftArrowPos = 1.5 * (canvas.width / 8);
+const upArrowPos = 2.75 * (canvas.width / 8);
+const downArrowPos = 4.0 * (canvas.width / 8);
+const rightArrowPos = 5.25 * (canvas.width / 8);
+
+const arrowWidth = 75;
+const arrowHeight = 75;
+const inputPadding = 15;
+const maxNotes = 12;
+
+function handleKeyPress(e) {
+    switch (e.keyCode) {
+        case 37:
+            leftArrowIsPressed = !leftArrowIsPressed;
+            break;
+        case 38:
+            upArrowIsPressed = !upArrowIsPressed;
+            break;
+        case 39:
+            rightArrowIsPressed = !rightArrowIsPressed;
+            break;
+        case 40:
+            downArrowIsPressed = !downArrowIsPressed;
+            break;
+    }
+}
 
 class Note {
-    constructor(){
-        this.x = 0;
-        this.y =0 ;
+
+    constructor(direction) {
+
+        switch (direction) {
+            case 'left':
+                this.x = leftArrowPos;
+                this.img = arrowLeft;
+                this.secondaryImg = greenLeft;
+                break;
+            case 'up':
+                this.x = upArrowPos;
+                this.img = arrowUp;
+                this.secondaryImg = greenUp;
+                break;
+            case 'down':
+                this.x = downArrowPos;
+                this.img = arrowDown;
+                this.secondaryImg = greenDown;
+                break;
+            case 'right':
+                this.x = rightArrowPos;
+                this.img = arrowRight;
+                this.secondaryImg = greenRight;
+                break;
+        }
+
+        this.validated = false;
+        this.y = 0;
+        this.direction = direction;
     }
 
-    updatePosition(){
-        this.y -= 20;
+    updatePosition() {
+        this.y += 2;
     }
 
-    draw(){
-
+    draw() {
+        ctx.drawImage(this.validated ? this.secondaryImg : this.img, this.x, this.y, arrowWidth, arrowHeight)
     }
 }
 
-function animationLoop(){
-  
-    // draw placeholder arrows
-
-    // update and draw moving arrows
-}
-
-function startGame(){
-
+function createNote() {
     if(!gameOn){
-        gameOn = true;
-        // start audio
-        const audioElement = new Audio("./assets/Vitas - 7th Element (2002) _D.mp3.mp3");
-        audioElement.play();
+        return;
     }
+    let directions = ['left', 'up', 'down', 'right'];
+    let noteDirection = directions[Math.floor(Math.random() * directions.length)]
+    let randomSeed = Math.floor(Math.random() * 2.2)
+    notesArray.push(new Note(noteDirection))
+    if(notesArray.length > maxNotes){
+        randomSeed *= 4;
+    }
+    setTimeout(() => {
+        createNote()
+    }, randomSeed*1000)       
+    
+}
+
+function validateNote(note) {
+    switch (note.direction) {
+        case 'left':
+            return leftArrowIsPressed;
+        case 'up':
+            return upArrowIsPressed;
+        case 'down':
+            return downArrowIsPressed;
+        case 'right':
+            return rightArrowIsPressed;
+    }
+}
+
+function updateScore(){
+    document.getElementById('score').innerHTML= score;
+}
+
+function animationLoop() {
+    // clear rect
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // draw placeholder arrows
+    ctx.drawImage(arrowLeft, leftArrowPos, canvas.height - arrowHeight, leftArrowIsPressed ? 4 + arrowWidth : arrowWidth, leftArrowIsPressed ? 4 + arrowHeight : arrowHeight)
+    ctx.drawImage(arrowUp, upArrowPos, canvas.height - arrowHeight, upArrowIsPressed ? 4 + arrowWidth : arrowWidth, upArrowIsPressed ? 4 + arrowHeight : arrowHeight)
+    ctx.drawImage(arrowDown, downArrowPos, canvas.height - arrowHeight, downArrowIsPressed ? 4 + arrowWidth : arrowWidth, downArrowIsPressed ? 4 + arrowHeight : arrowHeight)
+    ctx.drawImage(arrowRight, rightArrowPos, canvas.height - arrowHeight, rightArrowIsPressed ? 4 + arrowWidth : arrowWidth, rightArrowIsPressed ? 4 + arrowHeight : arrowHeight)
+    // update and draw moving arrows
+    notesArray.forEach((note, i, arr) => {
+        note.updatePosition();
+        note.draw();
+        if (note.y > canvas.height - arrowHeight - inputPadding && note.y < canvas.height - arrowHeight + inputPadding) {
+            if (note.validated === false && validateNote(note)) { // start checking 10 pixels before
+                note.validated = true;
+                score += 100;
+                updateScore();
+            }
+        }
+        if (note.y > canvas.height) { // clear the note
+            if (note.validated === false) {
+                score -= 50;
+                updateScore();
+            }
+            arr.splice(i, 1);
+        }
+    })
 
 }
 
+function pauseGame() {
+    audioElement.pause();
+    clearInterval(animationInterval);
+    gameOn = false;
+}
 
-window.onload = function() {
-    document.getElementById("start-button").onclick = function() {
-        startGame();
+function startGame() {
+
+    const startButton = document.getElementById("start-button");
+
+    if (!gameOn) {
+        gameOn = true;
+
+        startButton.innerHTML = `
+         <i class="fa-solid fa-pause"></i>
+        `
+        // start audio
+
+        audioElement.play();
+
+        animationInterval = setInterval(() => {
+            animationLoop();
+        }, 16);
+
+        createNote();
+
+        /*
+        addNotesInterval = setInterval(() => {
+            createNote();
+        }, 750);*/
+
+    } else {
+        startButton.innerHTML = `
+        <i class="fa-solid fa-play"></i>
+       `
+        pauseGame();
     }
+
+}
+/*
+function toggleMute() {
+    muted = !muted;
+    const muteButton = document.getElementById("mute");
+    if (muted) {
+        audioElement.pause();
+        muteButton.innerHTML = `
+         <i class="fa-solid fa-volume-high"></i>
+        `
+    } else {
+        audioElement.play();
+        muteButton.innerHTML = `
+         <i class="fa-solid fa-volume-xmark"></i>
+        `
+    }
+}*/
+
+window.onload = function () {
+    document.getElementById("start-button").onclick = startGame;
+    //document.getElementById("mute").onclick = toggleMute;
+    document.addEventListener("keydown", handleKeyPress);
+    document.addEventListener("keyup", handleKeyPress);
 }
