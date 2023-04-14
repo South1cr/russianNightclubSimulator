25eef615431ef692c564d93528f22880e30ced3e
Connor Southwell <south1cr@gmail.com>
2023-04-14 09:09:10 -0400

    final

--- js/index.js
+++ js/index.js
@@ -20,24 +20,27 @@ const arrowLeft = new Image();
 arrowLeft.src = './assets/staticLeft.png';
 const greenLeft = new Image();
 greenLeft.src = './assets/greenLeft.png';
+const purpleLeft = new Image();
+purpleLeft.src = './assets/purpleLeft.png';
 const arrowUp = new Image();
 arrowUp.src = './assets/staticUp.png';
 const greenUp = new Image();
 greenUp.src = './assets/greenUp.png';
+const purpleUp = new Image();
+purpleUp.src = './assets/purpleUp.png';
 const arrowDown = new Image();
 arrowDown.src = './assets/staticDown.png';
 const greenDown = new Image();
 greenDown.src = './assets/greenDown.png';
+const purpleDown = new Image();
+purpleDown.src = './assets/purpleDown.png';
 const arrowRight = new Image();
 arrowRight.src = './assets/staticRight.png';
 const greenRight = new Image();
 greenRight.src = './assets/greenRight.png';
-/*
-const arrowLeftDynamic = new Image()
-arrowLeftDynamic.src = './assets/leftArrowDynamic.png'
-const arrowUpDynamic = new Image();
-arrowUpDynamic.src = "./assets/arrowBlue.gif";
-*/
+const purpleRight = new Image();
+purpleRight.src = './assets/purpleRight.png';
+
 const arrowsArray = [];
 const combosArray = [];
 
@@ -49,21 +52,46 @@ const rightArrowPos = 5.25 * (canvas.width / 8);
 const arrowWidth = 75;
 const arrowHeight = 75;
 const inputPadding = 15;
-const maxArrows = 12;
+const maxArrows = 4;
 
-function handleKeyPress(e) {
+function handleKeyDown(e) {
+    if(["ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
     switch (e.keyCode) {
         case 37:
-            leftArrowIsPressed = !leftArrowIsPressed;
+            decrementScore()
+            leftArrowIsPressed = true;
             break;
         case 38:
-            upArrowIsPressed = !upArrowIsPressed;
+            decrementScore()
+            upArrowIsPressed = true;
             break;
         case 39:
-            rightArrowIsPressed = !rightArrowIsPressed;
+            decrementScore()
+            rightArrowIsPressed = true;
             break;
         case 40:
-            downArrowIsPressed = !downArrowIsPressed;
+            decrementScore()
+            downArrowIsPressed = true;
+            break;
+    }
+}
+
+
+function handleKeyUp(e) {
+    switch (e.keyCode) {
+        case 37:
+            leftArrowIsPressed = false;
+            break;
+        case 38:
+            upArrowIsPressed = false;
+            break;
+        case 39:
+            rightArrowIsPressed = false;
+            break;
+        case 40:
+            downArrowIsPressed = false;
             break;
     }
 }
@@ -74,22 +102,22 @@ class ArrowSprite {
         switch (direction) {
             case 'left':
                 this.x = leftArrowPos;
-                this.img = arrowLeft;
+                this.img = purpleLeft;
                 this.secondaryImg = greenLeft;
                 break;
             case 'up':
                 this.x = upArrowPos;
-                this.img = arrowUp;
+                this.img = purpleUp;
                 this.secondaryImg = greenUp;
                 break;
             case 'down':
                 this.x = downArrowPos;
-                this.img = arrowDown;
+                this.img = purpleDown;
                 this.secondaryImg = greenDown;
                 break;
             case 'right':
                 this.x = rightArrowPos;
-                this.img = arrowRight;
+                this.img = purpleRight;
                 this.secondaryImg = greenRight;
                 break;
         }
@@ -100,7 +128,11 @@ class ArrowSprite {
     }
 
     updatePosition() {
-        this.y += 1.5;
+        if(this.validated){
+            this.y += .8;
+        } else {
+            this.y += 1.5;
+        }
     }
 
     draw() {
@@ -108,28 +140,27 @@ class ArrowSprite {
     }
 }
 
-function createArrow() {
+function createArrowSprite() {
     if(!gameOn){
         return;
     }
     let directions = ['left', 'up', 'down', 'right'];
     let arrowDirection = directions[Math.floor(Math.random() * directions.length)]
     let randomSeed = Math.floor(Math.random() * 2.2)
-    arrowsArray.push(new ArrowSprite(arrowDirection))
-    if(arrowsArray.length > maxArrows){
-        randomSeed *= 4;
+    if(arrowsArray.length < maxArrows){
+        arrowsArray.push(new ArrowSprite(arrowDirection))
     }
     setTimeout(() => {
-        createArrow()
+        createArrowSprite()
     }, randomSeed*1000)       
     
 }  
 
-class ComboSprite {
-    constructor(combo, xPos){
-        this.combo = combo;
+class ScoreSprite {
+    constructor(score, xPos){
+        this.score = score;
         this.x=  xPos + arrowWidth / 2;
-        this.y = canvas.height - arrowHeight;
+        this.y = canvas.height - arrowHeight * 1.5;
     }
 
     updatePosition(){
@@ -139,16 +170,25 @@ class ComboSprite {
     draw(){
         ctx.fillStyle = '#eef577';
         ctx.font = '18px sans-serif';
-        ctx.fillText(`${this.combo}X`, this.x, this.y);
+        ctx.fillText(`${this.score}`, this.x, this.y);
     }   
 }
 
-function createCombo(combo, xPos){
-    combosArray.push(new ComboSprite(combo, xPos));
+function createScoreSprite(score, xPos){
+    combosArray.push(new ScoreSprite(score, xPos));
 }
 
-function validateArrow(arrow) {
-    switch (arrow.direction) {
+function decrementScore(arrow){
+    /*if(arrow){
+        createScoreSprite(-100, arrow.x)
+    }*/
+    if(score > 0){
+        score -= 100;
+    }
+}
+
+function validateArrow(direction) {
+    switch (direction) {
         case 'left':
             return leftArrowIsPressed;
         case 'up':
@@ -159,10 +199,6 @@ function validateArrow(arrow) {
             return rightArrowIsPressed;
     }
 }
-/*
-function updateScore(){
-    document.getElementById('score').innerHTML= score;
-}*/
 
 function animationLoop() {
     // clear rect
@@ -170,37 +206,34 @@ function animationLoop() {
     // draw combo and score 
     ctx.fillStyle = '#eef577';
     ctx.font = '18px sans-serif';
-    ctx.fillText(`Combo: ${scoreMultiplier}X  Score: ${score}`, canvas.width/3.1, 30);
+    ctx.fillText(`Combo: ${scoreMultiplier}X  Score: ${score}`, canvas.width/3.2, 30);
     // draw placeholder arrows
-    ctx.drawImage(arrowLeft, leftArrowPos, canvas.height - arrowHeight, leftArrowIsPressed ? 4 + arrowWidth : arrowWidth, leftArrowIsPressed ? 4 + arrowHeight : arrowHeight)
-    ctx.drawImage(arrowUp, upArrowPos, canvas.height - arrowHeight, upArrowIsPressed ? 4 + arrowWidth : arrowWidth, upArrowIsPressed ? 4 + arrowHeight : arrowHeight)
-    ctx.drawImage(arrowDown, downArrowPos, canvas.height - arrowHeight, downArrowIsPressed ? 4 + arrowWidth : arrowWidth, downArrowIsPressed ? 4 + arrowHeight : arrowHeight)
-    ctx.drawImage(arrowRight, rightArrowPos, canvas.height - arrowHeight, rightArrowIsPressed ? 4 + arrowWidth : arrowWidth, rightArrowIsPressed ? 4 + arrowHeight : arrowHeight)
+    ctx.drawImage(arrowLeft, leftArrowPos, canvas.height - arrowHeight * 1.5, leftArrowIsPressed ? 4 + arrowWidth : arrowWidth, leftArrowIsPressed ? 4 + arrowHeight : arrowHeight)
+    ctx.drawImage(arrowUp, upArrowPos, canvas.height - arrowHeight * 1.5, upArrowIsPressed ? 4 + arrowWidth : arrowWidth, upArrowIsPressed ? 4 + arrowHeight : arrowHeight)
+    ctx.drawImage(arrowDown, downArrowPos, canvas.height - arrowHeight * 1.5, downArrowIsPressed ? 4 + arrowWidth : arrowWidth, downArrowIsPressed ? 4 + arrowHeight : arrowHeight)
+    ctx.drawImage(arrowRight, rightArrowPos, canvas.height - arrowHeight * 1.5, rightArrowIsPressed ? 4 + arrowWidth : arrowWidth, rightArrowIsPressed ? 4 + arrowHeight : arrowHeight)
     // update and draw moving arrows
     arrowsArray.forEach((arrow, i, arr) => {
         arrow.updatePosition();
         arrow.draw();
-        if (arrow.y > canvas.height - arrowHeight - inputPadding && arrow.y < canvas.height - arrowHeight + inputPadding) {
-            if (arrow.validated === false && validateArrow(arrow)) { // start checking 10 pixels before
+        if (arrow.y > canvas.height - (arrowHeight * 1.5) - inputPadding && arrow.y < canvas.height - (arrowHeight * 1.5) + inputPadding) {
+            if (arrow.validated === false && validateArrow(arrow.direction)) { // start checking 10 pixels before
                 arrow.validated = true;
-                score += 100 * scoreMultiplier;
+                score += 100 + 100 * scoreMultiplier;
                 streak +=1;
+                createScoreSprite(100 * scoreMultiplier, arrow.x)
                 if(streak > 10){
-                    createCombo(3, arrow.x)
                     scoreMultiplier = 3;
                 } else if(streak > 5){
-                    createCombo(2, arrow.x)
                     scoreMultiplier = 2;
-                } else{
+                } else {
                     scoreMultiplier = 1;
                 }
             }
         }
         if (arrow.y > canvas.height) { // clear the arrow
             if (arrow.validated === false) {
-                if(score > 0){
-                    score -= 50;
-                }
+                decrementScore(arrow)
                 scoreMultiplier = 1;
                 streak = 0;
             }
@@ -212,7 +245,7 @@ function animationLoop() {
     combosArray.forEach((combo, i, arr) => {
         combo.updatePosition();
         combo.draw();
-        if (combo.y > canvas.height) { // clear the arrow
+        if (combo.y > canvas.height) { // clear the combo
             arr.splice(i, 1);
         }
     })
@@ -223,6 +256,30 @@ function pauseGame() {
     audioElement.pause();
     clearInterval(animationInterval);
     gameOn = false;
+}
+
+function muteGame(){
+
+    const muteButton = document.getElementById("mute-button");
+
+    if(muted){
+        muteButton.innerHTML = `
+        <i class="fa-solid fa-volume-xmark"></i>
+       `;   
+       audioElement.volume = 1;
+       muted = false;
+    } else {
+        muteButton.innerHTML = `
+        <i class="fa-solid fa-volume-high"></i>
+       `;   
+       audioElement.volume = 0;
+        muted = true;
+    }
+
+}
+
+function resetGame(){
+    window.location.reload();
 }
 
 function startGame() {
@@ -243,7 +300,7 @@ function startGame() {
             animationLoop();
         }, 4);
 
-        createArrow();
+        createArrowSprite();
 
     } else {
         startButton.innerHTML = `
@@ -256,6 +313,8 @@ function startGame() {
 
 window.onload = function () {
     document.getElementById("start-button").onclick = startGame;
-    document.addEventListener("keydown", handleKeyPress);
-    document.addEventListener("keyup", handleKeyPress);
+    document.getElementById("mute-button").onclick = muteGame;
+    document.getElementById("reset-button").onclick = resetGame;
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
 }
